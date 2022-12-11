698fa0d136043922c947d44bb848134da4dd831d
Avet90 <avet.sarkisian90@icloud.com>
2022-12-11 19:51:16 +0300

    constructor

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import AppHeader from '../AppHeader/AppHeader';
 import appStyles from '../App/App.module.css'
+import BurgerIngredients from '../BurgerIngredients/BurgerIngredients'
+import BurgerConstructor from '../BurgerConstructor/BurgerConstructor'
 
 
 class App extends React.Component {
@@ -10,6 +12,8 @@ class App extends React.Component {
       <div className={appStyles.page}>
         <AppHeader />
         <section className={appStyles.wrapper}>
+          <BurgerIngredients />
+          <BurgerConstructor/>
         </section>
       </div>
     )
